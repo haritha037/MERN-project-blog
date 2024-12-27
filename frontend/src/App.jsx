1080eaa1c3e3235dfb69fa7dbfc622e60eabcc67
haritha037 <e20037@eng.pdn.ac.lk>
2024-12-27 20:34:47 +0530

    Implemented the frontend routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,35 @@
-import { useEffect, useState } from "react";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import { getAllPostsApi } from "./api/postsApi";
+import LandingPage from "./pages/LandingPage";
+import Home from "./pages/Home";
+import CreateBlog from "./pages/CreateBlog";
+import ReadBlog from "./pages/ReadBlog";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function App() {
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    async function loadAllPosts() {
-      const data = await getAllPostsApi();
-
-      if (data) {
-        setPosts(data);
-      }
-    }
-
-    loadAllPosts();
-  }, []);
-
-  return <div>{JSON.stringify(posts)}</div>;
+  // Pages
+  // Landing page
+  // Home page(filtered by recency)
+  // ReadBlog
+  // CreateBlog
+  // Profile
+  // About
+  // Contact
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/create-blog" element={<CreateBlog />} />
+        <Route path="/read-blog/:id" element={<ReadBlog />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
